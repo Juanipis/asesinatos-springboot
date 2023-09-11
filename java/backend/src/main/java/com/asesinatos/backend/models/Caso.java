ace369e48393aa4fa28ef4923b0f6b17db221c29
natynaro <90767304+natynaro@users.noreply.github.com>
2023-09-11 14:38:29 -0500

    Cositas

--- java/backend/src/main/java/com/asesinatos/backend/models/Caso.java
+++ java/backend/src/main/java/com/asesinatos/backend/models/Caso.java
@@ -2,6 +2,7 @@ package com.asesinatos.backend.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -32,15 +33,15 @@ public class Caso {
      * 
      */
     
-     @OneToMany
+     @OneToMany(fetch = FetchType.EAGER)
      @JoinColumn(name = "persona_id")
      private Persona asesinado_id;
 
-     @OneToMany
+     @OneToMany(fetch = FetchType.EAGER)
      @JoinColumn(name = "persona_id")
      private Persona asesino_id;
 
-      @OneToMany
+      @OneToMany(mappedBy = "casos")
      @JoinColumn(name = "forma_asesinato_id")
      private FormaAsesinato forma_id;
 }
--- java/backend/src/main/java/com/asesinatos/backend/models/FormaAsesinato.java
+++ java/backend/src/main/java/com/asesinatos/backend/models/FormaAsesinato.java
@@ -5,6 +5,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,4 +18,8 @@ public class FormaAsesinato {
 
     @Column
     private String forma;
+
+    @ManyToOne
+    @JoinColumn(name = "casos_id")
+    private Caso caso;
 }
